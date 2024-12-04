3d2bd9eeb5e346929cb235ed731be81c79021d44
GDbateaux <yolan.savioz@hes-so.ch>
2024-12-04 21:23:58 +0100

    Fix pawn movement

--- Bots/FirstChessBot.py
+++ Bots/FirstChessBot.py
@@ -14,24 +14,27 @@ register_chess_bot('FirstBot', chess_bot)
 
 def movement_piece(board, x, y):
     if board[x,y] == '':
-        return
+        return []
     current_color = board[x,y][-1]
 
     def get_movement_pawn():
         res = []
-        moves = [
-             (1, 0)
-        ]
-        for dx, dy in moves:
-            nx, ny = x + dx, y + dy
-
-            if 0 <= nx < board.shape[0] and 0 <= ny < board.shape[1]:
-                next_new_mov = board[nx, ny]
-                if next_new_mov == '' or next_new_mov[-1] != current_color:
-                    res.append((nx, ny))
+        if x + 1 >= board.shape[0]:
+            return res
+        
+        new_x = x + 1
+        for i in range(3):
+            new_y = y + i - 1
+            if 0 <= new_y < board.shape[1]:
+                if i == 1:
+                    if board[new_x, new_y] == '':
+                        res.append((new_x, new_y))
+                else:
+                    if board[new_x, new_y] != '' and board[new_x, new_y][-1] != current_color:
+                        res.append((new_x, new_y))
         return res
 
-    def get_movement_rock():
+    def get_movement_rook():
         res = []
         directions = [(-1,0), (1, 0), (0, -1), (0, 1)]
 
@@ -97,7 +100,7 @@ def movement_piece(board, x, y):
         return res
     
     def get_movement_queen():
-        return get_movement_rock() + get_movement_bishop()
+        return get_movement_rook() + get_movement_bishop()
     
     def get_movement_king():
         res = []
@@ -117,22 +120,17 @@ def movement_piece(board, x, y):
     
     match board[x][y][0]:
         case 'p':
-            print('pawn')
             return get_movement_pawn()
         case 'r':
-            print('rock')
-            return get_movement_rock()
+            return get_movement_rook()
         case 'n':
-            print('knight')
             return get_movement_knight()
         case 'b':
-            print('bishop')
             return get_movement_bishop()
         case 'q':
-            print('queen')
             return get_movement_queen()
         case 'k':
-            print('king')
             return get_movement_king()
         case _:
             print('no valid piece')
+            return []
