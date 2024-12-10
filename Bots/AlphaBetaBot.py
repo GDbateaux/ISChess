797db1af5f16e8ca6bcd2729a0a2957bb6fad683
GDbateaux <yolan.savioz@hes-so.ch>
2024-12-10 21:56:29 +0100

    alphabetabot

---
+++ Bots/AlphaBetaBot.py
@@ -0,0 +1,41 @@
+from Bots.ChessBotList import register_chess_bot
+from .utils import Board, Move
+from .Evaluate import evaluate_v2
+
+
+def chess_bot(player_sequence, board, time_budget, **kwargs):
+    color = player_sequence[1]
+    board: Board = Board(board, color)
+    best_move:Move = alpha_beta(board, float('-inf'), float('inf'), 4)[1]
+    return best_move.get_return_move()
+
+
+def alpha_beta(board: Board, alpha, beta, depth):
+    if depth == 0:
+        return evaluate_v2(board), None
+
+    is_maximizing = board.board_color_top == board.color_to_play
+    best_evaluation = float('-inf') if is_maximizing else float('inf')
+    best_move = None
+
+    for move in board.get_movements():
+        board.make_move(move)
+        evaluation, _ = alpha_beta(board, alpha, beta, depth-1)
+        board.undo_move(move)
+
+        if is_maximizing:
+            if evaluation > best_evaluation:
+                best_evaluation = evaluation
+                best_move = move
+            alpha = max(alpha, evaluation)
+        elif not is_maximizing:
+            if evaluation < best_evaluation:
+                best_evaluation = evaluation
+                best_move = move
+            beta = min(beta, evaluation)
+        if beta < alpha:
+                break
+    
+    return best_evaluation, best_move
+
+register_chess_bot('AlphaBetaBot', chess_bot)
