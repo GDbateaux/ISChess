df03e8f611e353616ad98f8550fb58cb4e7b1ef2
FlorianHESWALLIS <153815619+FlorianHESWALLIS@users.noreply.github.com>
2024-12-12 13:22:23 +0100

    alphabetabotMemo
    FIX du problème du dépassement du temps

---
+++ Bots/AlphaBetaBotRandom.py
@@ -0,0 +1,67 @@
+from Bots.ChessBotList import register_chess_bot
+from .utils import Board, Move
+from .Evaluate import evaluate_v2
+import hashlib
+import time
+import random
+
+
+def chess_bot(player_sequence, board, time_budget, **kwargs):
+    start = time.time()
+    color = player_sequence[1]
+    board: Board = Board(board, color)
+    best_move: Move = alpha_beta_Random(board, float('-inf'), float('inf'), 4,start,time_budget-0.05)[1]
+    return best_move.get_return_move()
+
+def alpha_beta_Random(board: Board, alpha, beta, depth,start_time,time_limit):
+
+    is_maximizing = board.board_color_top == board.color_to_play
+    best_evaluation = float('-inf') if is_maximizing else float('inf')
+    best_move = None
+
+    list_random = board.get_movements().copy()
+    size = len(list_random)
+    counter = size / 2
+
+    if time.time() - start_time > time_limit:
+        test = random.randint(0, size - 1)
+        move = list_random[test]
+        return best_evaluation, move
+
+
+    if depth == 0 or board.is_game_over:
+        return evaluate_v2(board), None
+
+
+    while counter > 0:
+        size2 = len(list_random)
+        test = random.randint(0, size2 - 1)
+        move = list_random[test]
+        list_random.remove(move)
+
+
+        board.make_move(move)
+        evaluation, _ = alpha_beta_Random(board, alpha, beta, depth - 1,start_time, time_limit)
+        board.undo_move(move)
+
+        if is_maximizing:
+            if evaluation > best_evaluation:
+                best_evaluation = evaluation
+                best_move = move
+            alpha = max(alpha, evaluation)
+        elif not is_maximizing:
+            if evaluation < best_evaluation:
+                best_evaluation = evaluation
+                best_move = move
+            beta = min(beta, evaluation)
+        if beta < alpha:
+            break
+
+
+        counter -= 1
+
+    return best_evaluation, best_move
+
+
+
+register_chess_bot('AlphaBetaRandom', chess_bot)
