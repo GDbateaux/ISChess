6af94c0351ff4e74203d04af02a53de3870e240d
FlorianHESWALLIS <153815619+FlorianHESWALLIS@users.noreply.github.com>
2024-12-19 12:46:12 +0100

    Pour enregistrer les stats

--- Bots/AlphaBetaBotTimeMemo.py
+++ Bots/AlphaBetaBotTimeMemo.py
@@ -3,11 +3,28 @@ from Bots.utils import Board, Move
 from Bots.Evaluate import evaluate_v2
 from typing import List
 import time
+import csv
+import os
+
 
 
 def chess_bot(player_sequence, board, time_budget, **kwargs):
-    start = time.time()
+
+    #Pour les stats
+    counter_leaf = 0
+    csv_file = 'stat_result.csv'
+    file_exists = os.path.exists(csv_file)
+    with open(csv_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+
+        # Si le fichier n'existe pas, ajoutez l'en-tête
+        if not file_exists:
+            writer.writerow(['Player_Bot', 'Profondeur', 'Temps_recursion', 'Nb de Feuilles'])
+
+
+
     print(player_sequence)
+
     time_limit = time.time() + time_budget * 0.95
     color = player_sequence[1]
     board: Board = Board(board, color)
@@ -17,6 +34,9 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
     memoization = {}
 
     def alpha_beta(board: Board, alpha, beta, depth, time_limit):
+        # Pour les stats
+        nonlocal counter_leaf
+
         board_key = board.get_board_state()
         board_key_with_evaluate = (board_key, board.color_to_play)
 
@@ -29,6 +49,9 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
                 return memoization[board_key_with_evaluate], None
             else:
                 evaluation = evaluate_v2(board)
+                # Pour les stats
+                counter_leaf += 1
+
                 #print("Save evaluate")
                 memoization[board_key_with_evaluate] = evaluation
                 return evaluation, None
@@ -74,13 +97,31 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
     while (time_limit > time.time()):
         depth += 1
         try:
+
+            start = time.time()
+
             best_move = alpha_beta(board, float('-inf'), float('inf'), depth, time_limit)[1]
-            print(time.time() - start)
+
+            # Pour les stats
+            with open(csv_file, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['AlphaBetaBotTimeMemo', str(depth), str(time.time() - start), str(counter_leaf)])
+            counter_leaf = 0
+
         except TimeoutError:
             depth -= 1
             break
 
-    print(depth)
+
+
+
+
+
+
+    print("depth max :"+str(depth))
+
+
+
 
     return best_move.get_return_move()
 
